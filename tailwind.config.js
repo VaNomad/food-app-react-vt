c2d3888b294d57d6ca5727c1005e48d8116f5699
VaNomad <sjur.hassel@gmail.com>
2023-05-06 00:47:21 +0100

    Adds mobile menu and black overlay on toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "xx": "320px",
+        "xs": "420px",
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
+
   plugins: [],
 };
 
