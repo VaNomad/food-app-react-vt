1fbe40fcfe963d7d2ee964bf10d3e6bd90b19e3c
VaNomad <sjur.hassel@gmail.com>
2023-05-07 00:17:19 +0100

    Adds categories component w/styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import HeaderCards from "./components/HeaderCards";
 import Food from "./components/Food";
+import Category from "./components/Category";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Hero />
       <HeaderCards />
       <Food />
+      <Category />
     </div>
   );
 }
