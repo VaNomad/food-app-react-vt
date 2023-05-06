bddf9f376d8b2deaf7f87b1aa07c60dcc91db820
VaNomad <sjur.hassel@gmail.com>
2023-05-06 16:06:48 +0100

    Adds duration update to mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,7 +38,7 @@ export default function NavBar() {
       </div> : ""}
       
       {/* Side drawer menu */ }
-      <div className={nav ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300" : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-00"}>
+      <div className={nav ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300" : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-500"}>
         <AiOutlineClose onClick={() => setNav(!nav)} size={30} className="absolute right-4 top-4 cursor-pointer text-black" />
         <h2 className="text-black text-2xl p-4">
           Best <span className="font-bold">Eats</span>
