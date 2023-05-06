3905d5d512fdd5478fd88249eb9e19952de2201d
VaNomad <sjur.hassel@gmail.com>
2023-05-06 18:29:14 +0100

    Adds Food.jsx function + link + data.js array file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import HeaderCards from "./components/HeaderCards";
+import Food from "./components/Food";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <NavBar />
       <Hero />
       <HeaderCards />
+      <Food />
     </div>
   );
 }
